707c9363850d23b140fec96ca34ca33a004ed0ee
GuiCT <gtomiasi@gmail.com>
2023-06-05 21:30:56 -0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.css";
+</script>
+
+<style lang="scss">
+  :global(html) {
+    background-color: #161616;
+  }
+</style>
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/signUp/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let email = "";
+  let password = "";
+</script>
+
+<form class="flex flex-col items-end w-fit mx-auto my-10 justify-center gap-4">
+  <div class="flex flex-row gap-5">
+    <label class="text-white" for="emailInput">Email</label>
+    <input type="text" name="email" id="emailInput" bind:value={email} />    
+  </div>
+  <div class="flex flex-row gap-5">
+    <label class="text-white" for="passwordInput">Password</label>
+    <input type="text" name="password" id="passwordInput" bind:value={password} />    
+  </div>
+</form>
